ed57bdb0f7f59b5107dfc4e6380ad4e08b03ecf2
S.M Saleh Rabbi <sm.salehrabbi@gmail.com>
2018-05-05 12:15:20 +0600

    skill and counter

---
+++ js/custom.js
@@ -0,0 +1,23 @@
+//https://github.com/bfintal/Counter-Up
+
+jQuery(document).ready(function ($) {
+    $('.counter').counterUp({
+        delay: 10,
+        time: 1000
+    });
+});
+
+
+//https://www.jqueryscript.net/loading/Animated-Circle-Progress-Bar-with-jQuery-SVG-asPieProgress.html
+jQuery(document).ready(function ($) {
+    $('.pie_progress').asPieProgress({
+        'namespace': 'pie_progress',
+        barsize: '10',
+        trackcolor: '#e1e1e1',
+        barcolor: '#c51a08',
+    });
+    $('.pie_progress').asPieProgress('start');
+
+});
+
+
